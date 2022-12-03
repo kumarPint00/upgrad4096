e274db547c4d3d535978ea9714198e4a0fa80756
kumarpint00 <106054394+kumarpint0@users.noreply.github.com>
2022-12-03 20:38:56 +0530

    nodejs

---
+++ NodeJS/blockingNonBlocking.js
@@ -0,0 +1,18 @@
+// 'use strict'
+// console.log(Date.now().toString() + ': mainline: BEGIN');
+// const startTime = Date.now();
+// let endTime = startTime;
+// while (endTime < startTime + 20) {
+//     endTime = Date.now();
+// }
+// console.log(Date.now().toString() + ': mainline: END');
+
+
+
+
+// 'use strict'
+// console.log(Date.now().toString() + ': mainline: BEGIN');
+// setTimeout(() => {
+//     console.log(Date.now().toString() + ':event loop (callback): Asynchronous processing complete.');
+// }, 20);
+// console.log(Date.now().toString() + ':mainline: END');
---
+++ NodeJS/path.js
@@ -0,0 +1,28 @@
+getYear = function() {
+    return new Date().getFullYear();
+}
+getMonth = function() {
+    return new Date().getMonth();
+}
+module.exports = {getYear, getMonth};
+
+
+const date = {
+    getYear: function() {
+       return new Date().getFullYear();
+    },
+    getMonth: function() {
+       return new Date().getMonth();
+    }
+  }
+  module.exports = date;
+
+
+  const getYear = function() {
+    return new Date().getFullYear();
+  }
+  const getMonth = function() {
+    return new Date().getMonth();
+  }
+  module.exports.getYear = getYear;
+  module.exports.getMonth = getMonth;
